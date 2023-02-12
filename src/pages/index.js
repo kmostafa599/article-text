44bc5ff633d9aef6fbc107fba1ce3d78b688e4d6
kmostafa599 <kmostafa599@gmail.com>
2023-02-12 19:50:42 +0200

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -128,13 +128,3 @@ export default function Home() {
       </div>
     </div>)
 }
-// const CodeElement = props => {
-//   return (
-//     <pre {...props.attributes}>
-//       <code>{props.children}</code>
-//     </pre>
-//   )
-// }
-// const DefaultElement = props => {
-//   return <p {...props.attributes}>{props.children}</p>
-// }
